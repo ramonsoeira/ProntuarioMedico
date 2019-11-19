24f8a0a59d56f92452a4e93e046df777d6575188
MrAbade <mrabade@localhost.localdomain>
2019-11-19 16:06:48 -0300

    The function returns three different types of users, for now only one type is used, but the other two types of users will have future implementations.

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,7 +1,60 @@
 const mongoose = require('../../database')
 const bcrypt = require('bcryptjs')
 
-const userSchema = new mongoose.Schema({
+//ROOT ACCOUNT
+const userRootSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
+    ethAddress: {
+        type: String,
+        required: false,
+        select: false
+    },
+    medicalRegisterAddress: [{
+        type: String,
+        required: false,
+        select: false
+    }],
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    passwordResetToken: {
+        type: String,
+        required: false,
+        select: false
+    },
+    passwordResetExpires: {
+        type: String,
+        required: false,
+        select: false
+    },
+    createDate: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+userRootSchema.pre('save', async function(next) {
+    const hash = await bcrypt.hash(this.password, 10)
+    this.password = hash
+
+    next()
+})
+
+const UserRoot = mongoose.model('UserRoot', userRootSchema)
+
+//DOCTOR ACCOUNT
+const userDoctorSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
@@ -15,8 +68,7 @@ const userSchema = new mongoose.Schema({
     ethAddress: {
         type: String,
         unique: true,
-        select: false,
-        required: false
+        select: false
     },
     password: {
         type: String,
@@ -37,13 +89,65 @@ const userSchema = new mongoose.Schema({
     }
 })
 
-userSchema.pre('save', async function(next) {
+userDoctorSchema.pre('save', async function(next) {
     const hash = await bcrypt.hash(this.password, 10)
     this.password = hash
 
     next()
 })
 
-const User = mongoose.model('User', userSchema)
+const UserDoctor = mongoose.model('UserDoctor', userDoctorSchema)
 
-module.exports = User
+//Client Account
+
+const userClientSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
+    ethAddress: {
+        type: String,
+        unique: true,
+        select: false,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    passwordResetToken: {
+        type: String,
+        select: false
+    },
+    passwordResetExpires: {
+        type: String,
+        select: false
+    },
+    createDate: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+userClientSchema.pre('save', async function(next) {
+    const hash = await bcrypt.hash(this.password, 10)
+    this.password = hash
+
+    next()
+})
+
+const UserClient = mongoose.model('UserClient', userClientSchema)
+
+//EXPORT THE ACCOUNTS
+module.exports = {
+    UserRoot,
+    UserDoctor,
+    UserClient
+}
