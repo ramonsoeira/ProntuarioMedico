ffbd1dba12cfc7d792eacd3224fbd3287bf43f98
MrAbade <mrabade@localhost.localdomain>
2019-11-19 16:04:00 -0300

    Now the function passes two objects, app and web3

--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 const pathManag = require('path')
 
-module.exports = app => {
+module.exports = (app, web3) => {
     fs
     .readdirSync(__dirname)
     .filter(file => ((file.indexOf('.')) !== 0 && (file !== "index.js")))
-    .forEach(file => require(pathManag.resolve(__dirname, file))(app))
+    .forEach(file => require(pathManag.resolve(__dirname, file))(app, web3))
 }
